7812403d7766e5ca7955bc0037d97eaca24623d1
tonerkayla94 <tonerkayla94@gmail.com>
2021-12-02 14:39:12 -0500

    in progress

--- src/main/java/net/fabricmc/cooking/Main.java
+++ src/main/java/net/fabricmc/cooking/Main.java
@@ -1,19 +1,27 @@
 package net.fabricmc.cooking;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.block.Block;
 
 public class Main implements ModInitializer {
 
 	public static final Item FLOUR = new Item(new Item.Settings().group(ItemGroup.FOOD));
-		//flour.json png and name not appearing in game
+		//flour.json png not appearing in game
 	public static final Item SALT = new Item(new Item.Settings().group(ItemGroup.FOOD));
 	public static final Item DOUGH = new Item(new Item.Settings().group(ItemGroup.FOOD));
 	public static final Item BUTTER = new Item(new Item.Settings().group(ItemGroup.FOOD));
 	public static final Item CHEESE = new Item(new Item.Settings().group(ItemGroup.FOOD));
+	public static final Item CREAM = new Item(new Item.Settings().group(ItemGroup.FOOD));
+
+	//
 
 	@Override
 	public void onInitialize() {
@@ -27,5 +35,18 @@ public class Main implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("modid" , "dough"), DOUGH);
 		Registry.register(Registry.ITEM, new Identifier("modid" , "butter"), BUTTER);
 		Registry.register(Registry.ITEM, new Identifier("modid" , "cheese"), CHEESE);
+		Registry.register(Registry.ITEM, new Identifier("modid", "cream"), CREAM);
 	}
 }
+
+// salt ore
+//
+
+
+
+
+
+
+
+
+
